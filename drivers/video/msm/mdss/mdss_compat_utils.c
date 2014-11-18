77b516aaa1549fbae70922c1e2bacef028dda48c
Anusha Koduru <kanusha@codeaurora.org>
2014-11-18 15:44:53 +0530

    msm: mdss: Fix Compact Ioctls overlay_set failure
    Fix Compact Ioctls overlay_set failures due to improper handling of
    color_space parameter.
    
    Change-Id: I27f9b5b08d92a06bb0b8c4131ea11667ab924421
    Signed-off-by: Anusha Koduru <kanusha@codeaurora.org>

--- drivers/video/msm/mdss/mdss_compat_utils.c
+++ drivers/video/msm/mdss/mdss_compat_utils.c
@@ -2248,6 +2248,11 @@ static int __to_user_mdp_overlay(struct mdp_overlay32 __user *ov32,
 			   sizeof(struct mdp_scale_data));
 	if (ret)
 		return -EFAULT;
+
+	ret |= put_user(ov->color_space, &ov32->color_space);
+	if (ret)
+		return -EFAULT;
+
 	return 0;
 }
 
@@ -2302,6 +2307,10 @@ static int __from_user_mdp_overlay(struct mdp_overlay *ov,
 
 	if (copy_in_user(&ov->scale, &ov32->scale,
 			 sizeof(struct mdp_scale_data)))
+		return -EFAULT;
+
+	if (get_user(data, &ov32->color_space) ||
+	    put_user(data, &ov->color_space))
 		return -EFAULT;
 
 	return 0;
