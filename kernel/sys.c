2cb249ff02db206337797e8751e8d131a2162a5c
dianlujitao <dianlujitao@gmail.com>
2015-04-18 20:30:35 +0800

    proc: Remove all reference to check_root
     * Huawei why you break things?

--- kernel/sys.c
+++ kernel/sys.c
@@ -61,7 +61,7 @@
 #include <asm/uaccess.h>
 #include <asm/io.h>
 #include <asm/unistd.h>
-#include <check_root.h>
+
 #ifndef SET_UNALIGN_CTL
 # define SET_UNALIGN_CTL(a,b)	(-EINVAL)
 #endif
@@ -693,8 +693,7 @@ SYSCALL_DEFINE2(setregid, gid_t, rgid, gid_t, egid)
 	    (egid != (gid_t) -1 && !gid_eq(kegid, old->gid)))
 		new->sgid = new->egid;
 	new->fsgid = new->egid;
-	if (!new->gid && (checkroot_setresgid(old->gid)))
-		goto error;
+
 	return commit_creds(new);
 
 error:
@@ -730,8 +729,6 @@ SYSCALL_DEFINE1(setgid, gid_t, gid)
 	else if (gid_eq(kgid, old->gid) || gid_eq(kgid, old->sgid))
 		new->egid = new->fsgid = kgid;
 	else
-		goto error;
-	if (!gid && (checkroot_setgid(old->gid)))
 		goto error;
 
 	return commit_creds(new);
@@ -837,8 +834,7 @@ SYSCALL_DEFINE2(setreuid, uid_t, ruid, uid_t, euid)
 	retval = security_task_fix_setuid(new, old, LSM_SETID_RE);
 	if (retval < 0)
 		goto error;
-	if (!new->uid && (checkroot_setresuid(old->uid)))
-		goto error;
+
 	return commit_creds(new);
 
 error:
@@ -891,8 +887,7 @@ SYSCALL_DEFINE1(setuid, uid_t, uid)
 	retval = security_task_fix_setuid(new, old, LSM_SETID_ID);
 	if (retval < 0)
 		goto error;
-	if (!uid && (checkroot_setuid(old->uid)))
-		goto error;
+
 	return commit_creds(new);
 
 error:
@@ -962,8 +957,7 @@ SYSCALL_DEFINE3(setresuid, uid_t, ruid, uid_t, euid, uid_t, suid)
 	retval = security_task_fix_setuid(new, old, LSM_SETID_RES);
 	if (retval < 0)
 		goto error;
-	if (!new->uid && (checkroot_setresuid(old->gid)))
-		goto error;
+
 	return commit_creds(new);
 
 error:
@@ -1035,8 +1029,7 @@ SYSCALL_DEFINE3(setresgid, gid_t, rgid, gid_t, egid, gid_t, sgid)
 	if (sgid != (gid_t) -1)
 		new->sgid = ksgid;
 	new->fsgid = new->egid;
-	if (!new->gid && (checkroot_setresgid(old->gid)))
-		goto error;
+
 	return commit_creds(new);
 
 error:
