0c896741a127c4a35d7be5e45a08309378e88d3a
dianlujitao <dianlujitao@gmail.com>
2015-08-31 23:00:26 +0800

    Revert "staging: ion: Optimize zeroing when allocating from system heap"
    This reverts commit 85038cfd2e3345a39e76c874356e9ff4b542d864.

--- drivers/staging/android/ion/ion_heap.c
+++ drivers/staging/android/ion/ion_heap.c
@@ -25,7 +25,6 @@
 #include <linux/vmalloc.h>
 #include <linux/slab.h>
 #include <linux/highmem.h>
-#include <linux/dma-mapping.h>
 #include "ion.h"
 #include "ion_priv.h"
 
@@ -113,15 +112,16 @@ int ion_heap_map_user(struct ion_heap *heap, struct ion_buffer *buffer,
  * chunks to minimize the number of memsets and vmaps/vunmaps.
  *
  * Note that the `pages' array should be composed of all 4K pages.
- *
- * NOTE: This function does not guarantee synchronization of the caches
- * and thus caller is responsible for handling any cache maintenance
- * operations needed.
  */
 int ion_heap_pages_zero(struct page **pages, int num_pages)
 {
-	int i, j, npages_to_vmap;
+	int i, j, k, npages_to_vmap;
 	void *ptr = NULL;
+	/*
+	 * It's cheaper just to use writecombine memory and skip the
+	 * cache vs. using a cache memory and trying to flush it afterwards
+	 */
+	pgprot_t pgprot = pgprot_writecombine(PAGE_KERNEL);
 
 	/*
 	 * As an optimization, we manually zero out all of the pages
@@ -137,7 +137,7 @@ int ion_heap_pages_zero(struct page **pages, int num_pages)
 		for (j = 0; j < MAX_VMAP_RETRIES && npages_to_vmap;
 			++j) {
 			ptr = vmap(&pages[i], npages_to_vmap,
-					VM_IOREMAP, PAGE_KERNEL);
+					VM_IOREMAP, pgprot);
 			if (ptr)
 				break;
 			else
@@ -146,6 +146,18 @@ int ion_heap_pages_zero(struct page **pages, int num_pages)
 		if (!ptr)
 			return -ENOMEM;
 
+		/*
+		 * We have to invalidate the cache here because there
+		 * might be dirty lines to these physical pages (which
+		 * we don't care about) that could get written out at
+		 * any moment.
+		 */
+		for (k = 0; k < npages_to_vmap; k++) {
+			void *p = kmap_atomic(pages[i + k]);
+
+			dmac_inv_range(p, p + PAGE_SIZE);
+			kunmap_atomic(p);
+		}
 		memset(ptr, 0, npages_to_vmap * PAGE_SIZE);
 		vunmap(ptr);
 	}
@@ -153,13 +165,11 @@ int ion_heap_pages_zero(struct page **pages, int num_pages)
 	return 0;
 }
 
-int ion_heap_alloc_pages_mem(struct pages_mem *pages_mem)
+static int ion_heap_alloc_pages_mem(int page_tbl_size,
+				struct pages_mem *pages_mem)
 {
 	struct page **pages;
-	unsigned int page_tbl_size;
-
 	pages_mem->free_fn = kfree;
-	page_tbl_size = sizeof(struct page *) * (pages_mem->size >> PAGE_SHIFT);
 	if (page_tbl_size > SZ_8K) {
 		/*
 		 * Do fallback to ensure we have a balance between
@@ -183,7 +193,7 @@ int ion_heap_alloc_pages_mem(struct pages_mem *pages_mem)
 	return 0;
 }
 
-void ion_heap_free_pages_mem(struct pages_mem *pages_mem)
+static void ion_heap_free_pages_mem(struct pages_mem *pages_mem)
 {
 	pages_mem->free_fn(pages_mem->pages);
 }
@@ -193,17 +203,15 @@ int ion_heap_high_order_page_zero(struct page *page, int order)
 	int i, ret;
 	struct pages_mem pages_mem;
 	int npages = 1 << order;
-	pages_mem.size = npages * PAGE_SIZE;
+	int page_tbl_size = sizeof(struct page *) * npages;
 
-	if (ion_heap_alloc_pages_mem(&pages_mem))
+	if (ion_heap_alloc_pages_mem(page_tbl_size, &pages_mem))
 		return -ENOMEM;
 
 	for (i = 0; i < (1 << order); ++i)
 		pages_mem.pages[i] = page + i;
 
 	ret = ion_heap_pages_zero(pages_mem.pages, npages);
-	dma_sync_single_for_device(NULL, page_to_phys(page), pages_mem.size,
-				   DMA_BIDIRECTIONAL);
 	ion_heap_free_pages_mem(&pages_mem);
 	return ret;
 }
@@ -212,12 +220,16 @@ int ion_heap_buffer_zero(struct ion_buffer *buffer)
 {
 	struct sg_table *table = buffer->sg_table;
 	struct scatterlist *sg;
-	int i, j, ret = 0, npages = 0;
+	int i, j, ret = 0, npages = 0, page_tbl_size = 0;
 	struct pages_mem pages_mem;
 
-	pages_mem.size = PAGE_ALIGN(buffer->size);
+	for_each_sg(table->sgl, sg, table->nents, i) {
+		unsigned long len = sg_dma_len(sg);
+		int nrpages = len >> PAGE_SHIFT;
+		page_tbl_size += sizeof(struct page *) * nrpages;
+	}
 
-	if (ion_heap_alloc_pages_mem(&pages_mem))
+	if (ion_heap_alloc_pages_mem(page_tbl_size, &pages_mem))
 		return -ENOMEM;
 
 	for_each_sg(table->sgl, sg, table->nents, i) {
@@ -229,8 +241,6 @@ int ion_heap_buffer_zero(struct ion_buffer *buffer)
 	}
 
 	ret = ion_heap_pages_zero(pages_mem.pages, npages);
-	dma_sync_sg_for_device(NULL, table->sgl, table->nents,
-			       DMA_BIDIRECTIONAL);
 	ion_heap_free_pages_mem(&pages_mem);
 	return ret;
 }
