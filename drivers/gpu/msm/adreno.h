2a289a585a6fdebe1970606dee6f921fe0f033cb
dianlujitao <dianlujitao@gmail.com>
2015-09-01 11:36:54 +0800

    Revert "msm: kgsl: Skip the entire command during fault recovery"
    This reverts commit 8dbb2950e0bf0d11349b70c31fcc9d6f33f0c5a6.

--- drivers/gpu/msm/adreno.h
+++ drivers/gpu/msm/adreno.h
@@ -555,7 +555,6 @@ struct log_field {
 #define  KGSL_FT_DISABLE                  4
 #define  KGSL_FT_TEMP_DISABLE             5
 #define  KGSL_FT_THROTTLE                 6
-#define  KGSL_FT_SKIPCMD                  7
 #define  KGSL_FT_DEFAULT_POLICY (BIT(KGSL_FT_REPLAY) + BIT(KGSL_FT_SKIPIB) \
 				+ BIT(KGSL_FT_THROTTLE))
 
@@ -576,8 +575,7 @@ struct log_field {
 	{ BIT(KGSL_FT_SKIPFRAME), "skipframe" }, \
 	{ BIT(KGSL_FT_DISABLE), "disable" }, \
 	{ BIT(KGSL_FT_TEMP_DISABLE), "temp" }, \
-	{ BIT(KGSL_FT_THROTTLE), "throttle"}, \
-	{ BIT(KGSL_FT_SKIPCMD), "skipcmd" }
+	{ BIT(KGSL_FT_THROTTLE), "throttle"}
 
 extern struct adreno_gpudev adreno_a3xx_gpudev;
 extern struct adreno_gpudev adreno_a4xx_gpudev;
@@ -641,10 +639,6 @@ void adreno_dispatcher_pause(struct adreno_device *adreno_dev);
 void adreno_dispatcher_queue_context(struct kgsl_device *device,
 	struct adreno_context *drawctxt);
 int adreno_reset(struct kgsl_device *device);
-
-void adreno_fault_skipcmd_detached(struct kgsl_device *device,
-					 struct adreno_context *drawctxt,
-					 struct kgsl_cmdbatch *cmdbatch);
 
 int adreno_perfcounter_get_groupid(struct adreno_device *adreno_dev,
 					const char *name);
